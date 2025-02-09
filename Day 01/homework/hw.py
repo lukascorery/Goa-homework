cafb24f1415424f553ab29833de72cfcf062ac06
lukascorery <lukashavadze2008@gmail.com>
2025-02-09 23:50:51 +0400

    first commit

---
+++ Day 01/homework/hw.py
@@ -0,0 +1,23 @@
+name = "luka"
+
+
+surname = "shavadze"
+
+#print(name, surname)
+
+story = "i love this, thank you nika keshelava "
+age = 17
+height = 179.6 
+#boolean = 17/9
+
+knows_programming = True
+doesnt_know_anything = False
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+
+#print(name + " " + str(age))
+
+print(name, surname, age, height, story)
