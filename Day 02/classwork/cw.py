add97867570014dd14c848bdba4dff52bac36d02
lukascorery <lukashavadze2008@gmail.com>
2025-02-12 20:40:35 +0400

    lukashavadze

---
+++ Day 02/classwork/cw.py
@@ -0,0 +1,11 @@
+name = "luka"
+surname = "shavadze"
+print(name + " " + surname)
+#syntax - ეს არის წერის ენა
+#variable - ცვლადი
+#equal sign - ტოლობის ნიშანი
+#variable name - ცვლადის სახელი
+#variable value - ცვლადის მნიშვნელობა
+#string - ტექსტური მონაცემი
+#parenthesis - ბრჩხილები
+#quotation marks -  ბრჭყალები
