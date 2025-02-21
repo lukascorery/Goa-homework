10956188be7d150b9aba5b8892165e9db37f3a75
lukascorery <lukashavadze2008@gmail.com>
2025-02-21 23:47:37 +0400

    second commit

---
+++ day 04/classwork/cw1.py
@@ -0,0 +1,13 @@
+print(5 + 5)
+print(10 - 5)
+print(10 / 2)
+print(10 * 2)
+
+sum = 10
+sum2 = 5
+sum3 = 5.0
+sum4 = 20
+print(sum)
+print(sum2)
+print(sum3)
+print(sum4)
---
+++ day 04/homework/hw.py
@@ -0,0 +1,7 @@
+me = "luka"
+mother = "mzevinari"
+father = "amirani"
+brother = "ioane"
+grandpa = "lutbie"
+
+print("me mqvia " + me + ". " + "dedachemis saxelia " + mother + " xolo mamachemis " + father + "." + " me aseve myavs dzma, mas hqvia " + brother + " da myavs bebia saxelad " + grandpa)
