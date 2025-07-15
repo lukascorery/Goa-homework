5f49a6aca12c1c723bab7cbc59f7dc37f982b7f6
lukascorery <lukashavadze2008@gmail.com>
2025-07-15 16:42:09 +0400

    lukashavadze

--- day 36/homework/code/codewars5.py
+++ day 36/homework/code/codewars5.py
@@ -1,1 +1,8 @@
 """https://www.codewars.com/kata/567bf4f7ee34510f69000032/train/python"""
+def is_digit(n):
+    if len(n) == 1:
+        for i in n:
+            if i.isdigit():
+                return True
+    return False
+
--- day 36/homework/code/codewars6.py
+++ day 36/homework/code/codewars6.py
@@ -1,1 +1,7 @@
 """https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1/train/python"""
+def capitalize(s, ind):
+    result = list(s)
+    for index in ind:
+        if index < len(result):
+            result[index] = result[index].upper()
+    return "".join(result)
